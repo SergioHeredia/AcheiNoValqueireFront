db1939771e566301d0f61b5a034a2e7fe36524cf
SergioHeredia <srpheredia@gmail.com>
2022-10-20 16:03:54 -0300

    Update api.ts

--- src/infra/api/api.ts
+++ src/infra/api/api.ts
@@ -5,7 +5,7 @@ import axios from "axios";
 
 // Create a instance of axios to use the same base url.
 const axiosAPI = axios.create({
-  baseURL : "https://localhost:7249/api/" // it's not recommended to have this info here.
+  baseURL : "http://localhost:5000/api/" // it's not recommended to have this info here.
 });
 
 axios.defaults.withCredentials = true;
