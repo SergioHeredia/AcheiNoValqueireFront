ad519419b8215aa23682deca749df3e69179d5f4
SergioHeredia <srpheredia@gmail.com>
2022-10-20 16:25:36 -0300

    Update http.ts

--- src/infra/api/http.ts
+++ src/infra/api/http.ts
@@ -3,5 +3,5 @@ import axios from "axios";
 export const API_KEY = "a1279933de606b4374a2c93a1d0127a9";
 
 export const http = axios.create({
-  baseURL: "http://localhost:5000/api/",
+  baseURL: "http://www.acheinovalqueire.com.br:5010/api/",
 });
